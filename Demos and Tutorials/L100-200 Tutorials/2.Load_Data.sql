07255128a08abbd482b06fafb2fa0a81a72fb495
saurabhshukla-db <saurabhshukla-db>
2024-05-05 01:34:09 +0000

    commit

--- Demos and Tutorials/L100-200 Tutorials/2.Load_Data.sql
+++ Demos and Tutorials/L100-200 Tutorials/2.Load_Data.sql
@@ -1,26 +1,33 @@
-USE CATALOG MAIN;
-USE SCHEMA DEFAULT;
+DECLARE OR REPLACE VARIABLE catalog_name STRING DEFAULT 'main';
+DECLARE OR REPLACE VARIABLE schema_name STRING DEFAULT 'default';
+
+SET VAR catalog_name = {{catalog}};
+SET VAR schema_name = {{schema}};
 
 -- 2/ Let's add some data to the Dimension Tables
 INSERT INTO
-  dim_store (store_name, address)
+  IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_store')  
+  (store_name, address)
 VALUES
   ('City Store', '1 Main Rd, Whoville');
   
 INSERT INTO
-  dim_product (sku, description, category)
+  IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_product')  
+  (sku, description, category)
 VALUES
   ('1000001', 'High Tops', 'Ladies Shoes'),
   ('7000003', 'Printed T', 'Ladies Fashion Tops');
   
 INSERT INTO
-  dim_customer (customer_name, customer_profile, address)
+  IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_customer')  
+  (customer_name, customer_profile, address)
 VALUES
   ('Al', 'Al profile', 'Databricks - Queensland Australia'),
   ('Quentin', 'REDACTED_PROFILE', 'Databricks - Paris France');
 -- 3/ Let's add some data to the Fact Tables
 INSERT INTO
-  fact_sales (product_id, store_id, customer_id, price_sold, units_sold, dollar_cost)
+  IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'fact_sales')  
+  (product_id, store_id, customer_id, price_sold, units_sold, dollar_cost)
 VALUES
   (1, 1, 0, 100.99, 2, 2.99),
   (2, 1, 0, 10.99, 2, 2.99),
--- Demos and Tutorials/L100-200 Tutorials/3.Query_Fact_Sales.sql
+++ Demos and Tutorials/L100-200 Tutorials/3.Query_Fact_Sales.sql
@@ -1,7 +1,3 @@
---USE CATALOG MAIN;
---USE SCHEMA DEFAULT;
-
-
 DECLARE OR REPLACE VARIABLE catalog_name STRING DEFAULT 'main';
 DECLARE OR REPLACE VARIABLE schema_name STRING DEFAULT 'default';
 
@@ -11,6 +7,6 @@ SET VAR schema_name = {{schema}};
 
 -- ### Query the tables joining data
 SELECT * FROM IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'fact_sales') 
-  INNER JOIN IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_product')   USING (product_id)
-  INNER JOIN IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_customer')  USING (customer_id)
-  INNER JOIN IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_store')     USING (store_id);
+   INNER JOIN IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_product')   USING (product_id)
+   INNER JOIN IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_customer')  USING (customer_id)
+   INNER JOIN IDENTIFIER(catalog_name || '.' || schema_name  || '.' || 'dim_store')     USING (store_id);
