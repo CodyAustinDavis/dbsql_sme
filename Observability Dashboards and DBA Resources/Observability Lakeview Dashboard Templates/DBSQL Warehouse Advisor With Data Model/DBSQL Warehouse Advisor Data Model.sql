2e7dfa641d10464d609bce93d493fb6c2c2482d9
Cody Austin Davis <cody.davis@databricks.com>
2024-09-15 12:13:33 -0700

    ANSI Compliant DBSQL Advisor
    - Make DBSQL Advisor model and dash ANSI compliant
    - Add QPS base metrics (count, total seconds)
    - Clean up visuals

--- Observability Dashboards and DBA Resources/Observability Lakeview Dashboard Templates/DBSQL Warehouse Advisor With Data Model/DBSQL Warehouse Advisor Data Model.sql
+++ Observability Dashboards and DBA Resources/Observability Lakeview Dashboard Templates/DBSQL Warehouse Advisor With Data Model/DBSQL Warehouse Advisor Data Model.sql
@@ -1,5 +1,4 @@
-
---DROP SCHEMA IF EXISTS main.dbsql_warehouse_advisor CASCADE;
+DROP SCHEMA IF EXISTS main.dbsql_warehouse_advisor CASCADE;
 CREATE SCHEMA IF NOT EXISTS main.dbsql_warehouse_advisor;
 USE CATALOG main;
 USE SCHEMA dbsql_warehouse_advisor;
@@ -12,72 +11,114 @@ TBLPROPERTIES ('pipelines.autoOptimize.zOrderCols'= 'warehouse_id,start_time')
 AS 
 (
 SELECT
-account_id, 
-workspace_id, 
-statement_id,
-executed_by,
-statement_text,
-compute.warehouse_id AS warehouse_id,
-execution_status,
-statement_type,
-COALESCE(client_application, 'Unknown') AS client_application,
-COALESCE(try_divide(total_duration_ms, 1000), 0) AS QueryRuntimeSeconds,
-COALESCE(try_divide(total_task_duration_ms, 1000), 0) AS CPUTotalExecutionTime,
-COALESCE(try_divide(execution_duration_ms, 1000), 0) AS ExecutionQueryTime, -- Included in Cost Per Query
-COALESCE(try_divide(compilation_duration_ms, 1000), 0) AS CompilationQueryTime, -- Included in Cost Per Query
-COALESCE(try_divide(waiting_at_capacity_duration_ms, 1000), 0) AS QueueQueryTime,
-COALESCE(try_divide(waiting_for_compute_duration_ms, 1000), 0) AS StartUpQueryTime,
-COALESCE(try_divide(result_fetch_duration_ms, 1000), 0) AS ResultFetchTime,
--- Metric for query cost allocation - -- exclude metadata operations
-CASE WHEN COALESCE(try_divide(total_task_duration_ms, 1000),0) = 0 
-    THEN 0 
-    ELSE COALESCE(try_divide(total_duration_ms, 1000), 0)  + COALESCE(try_divide(compilation_duration_ms, 1000), 0) -- Query total time is compile time + execution time
+    account_id, 
+    workspace_id, 
+    statement_id,
+    executed_by,
+    statement_text,
+    compute.warehouse_id AS warehouse_id,
+    execution_status,
+    statement_type,
+    COALESCE(client_application, 'Unknown') AS client_application,
+    COALESCE(CAST(total_duration_ms AS FLOAT) / 1000, 0) AS QueryRuntimeSeconds,
+    COALESCE(CAST(total_task_duration_ms AS FLOAT) / 1000, 0) AS CPUTotalExecutionTime,
+    COALESCE(CAST(execution_duration_ms AS FLOAT) / 1000, 0) AS ExecutionQueryTime, -- Included in Cost Per Query
+    COALESCE(CAST(compilation_duration_ms AS FLOAT) / 1000, 0) AS CompilationQueryTime, -- Included in Cost Per Query
+    COALESCE(CAST(waiting_at_capacity_duration_ms AS FLOAT) / 1000, 0) AS QueueQueryTime,
+    COALESCE(CAST(waiting_for_compute_duration_ms AS FLOAT) / 1000, 0) AS StartUpQueryTime,
+    COALESCE(CAST(result_fetch_duration_ms AS FLOAT) / 1000, 0) AS ResultFetchTime,
+    
+    -- Metric for query cost allocation - exclude metadata operations
+    CASE 
+        WHEN COALESCE(CAST(total_task_duration_ms AS FLOAT) / 1000, 0) = 0 
+        THEN 0 
+        ELSE COALESCE(CAST(total_duration_ms AS FLOAT) / 1000, 0) + COALESCE(CAST(compilation_duration_ms AS FLOAT) / 1000, 0) 
     END AS TotalResourceTimeUsedForAllocation,
-start_time,
-end_time,
-update_time,
-COALESCE(read_bytes, 0) AS read_bytes,
-COALESCE(written_bytes, 0) AS written_bytes, -- New!
-COALESCE(read_io_cache_percent, 0) AS read_io_cache_percent,
-from_result_cache,
-COALESCE(spilled_local_bytes, 0) AS spilled_local_bytes,
-COALESCE(total_task_duration_ms / total_duration_ms, NULL) AS TotalCPUTime_To_Execution_Time_Ratio, --execution time does seem to vary across query type, using total time to standardize
-COALESCE(waiting_at_capacity_duration_ms / total_duration_ms, 0) AS ProportionQueueTime,
-AVG(try_divide(total_duration_ms, 1000))  OVER () AS WarehouseAvgQueryRuntime,
-AVG(try_divide(waiting_at_capacity_duration_ms, 1000)) OVER () AS WarehouseAvgQueueTime,
-AVG(COALESCE(try_divide(waiting_at_capacity_duration_ms, 1000) / try_divide(total_duration_ms, 1000), 0)) OVER () AS WarehouseAvgProportionTimeQueueing,
--- Can use this to chargeback (as long as you know denominator is only USED task time, not including idele time)
-CASE WHEN read_bytes > 0 THEN try_divide(read_bytes,(1024*1024*1024))ELSE 0 END AS ReadDataAmountInGB,
-CASE WHEN read_io_cache_percent > 0 THEN 'Used Cache' ELSE 'No Cache' END AS UsedCacheFlag,
-CASE WHEN spilled_local_bytes > 0 THEN 'Spilled Data To Disk' ELSE 'No Spill' END AS HasSpillFlag,
-CASE WHEN read_bytes > 0 THEN 'Did Read Data' ELSE 'No Data Read' END AS ReadDataFlag,
-CASE WHEN CPUTotalExecutionTime > 0 THEN 'UsedWorkerTasks' ELSE 'NoWorkers' END AS UsedWorkerTasksFlag,
 
---CASE WHEN QueryRuntimeSeconds >= :long_running_seconds_treshold::float THEN 'Long Running' ELSE 'Short Running' END AS LongRunningQueryFlag,
-CASE WHEN spilled_local_bytes > 0 THEN 1 ELSE 0 END AS Calc_HasSpillFlag,
-CASE WHEN read_bytes > 0 THEN 0.25 ELSE 0 END AS Calc_ReadDataFlag,
-CASE WHEN CPUTotalExecutionTime > 0 THEN 0.25 ELSE 0 END AS Calc_UsedWorkerTasksFlag,
---CASE WHEN QueryRuntimeSeconds >= :long_running_seconds_treshold::float THEN 1 ELSE 0 END AS Calc_LongRunningQueryFlag,
--- Query Tagging 
-regexp_replace(
-regexp_extract(
-statement_text, 
-r'/\*.*?QUERY_TAG:(.*?)(?=\*/)',
-1
-), 
-'QUERY_TAG:',
-''
-) AS raw_tagged,
-substr(statement_text, INSTR(statement_text, '/*') + 2, INSTR(statement_text, '*/') - INSTR(statement_text, '/*') - 2) AS dbt_metadata_json,
--- Optional DBT Metadata
-COALESCE(dbt_metadata_json:app, 'None') AS dbt_app,
-COALESCE(dbt_metadata_json:node_id, 'None') AS dbt_node_id,
-COALESCE(dbt_metadata_json:profile_name, 'None') AS dbt_profile_name,
-COALESCE(dbt_metadata_json:target_name, 'None') AS dbt_target_name,
-COALESCE(dbt_metadata_json:dbt_version, 'None') AS dbt_version,
-COALESCE(dbt_metadata_json:dbt_databricks_version, 'None') AS dbt_databricks_version,
-from_json(dbt_metadata_json, 'map<string,string>') AS parsed_dbt_comment,
-CASE WHEN (lower(dbt_metadata_json:app) = "dbt" OR client_application LIKE ('%dbt%')) THEN 'DBT Query' ELSE 'Other Query Type' END AS IsDBTQuery
+    start_time,
+    end_time,
+    update_time,
+    COALESCE(read_bytes, 0) AS read_bytes,
+    COALESCE(written_bytes, 0) AS written_bytes, -- New!
+    COALESCE(read_io_cache_percent, 0) AS read_io_cache_percent,
+    from_result_cache,
+    COALESCE(spilled_local_bytes, 0) AS spilled_local_bytes,
+    
+    COALESCE(CAST(total_task_duration_ms AS FLOAT) / NULLIF(total_duration_ms, 0), NULL) AS TotalCPUTime_To_Execution_Time_Ratio, 
+    COALESCE(CAST(waiting_at_capacity_duration_ms AS FLOAT) / NULLIF(total_duration_ms, 0), 0) AS ProportionQueueTime,
+    
+    AVG(CAST(total_duration_ms AS FLOAT) / 1000) OVER () AS WarehouseAvgQueryRuntime,
+    AVG(CAST(waiting_at_capacity_duration_ms AS FLOAT) / 1000) OVER () AS WarehouseAvgQueueTime,
+    AVG(COALESCE(CAST(waiting_at_capacity_duration_ms AS FLOAT) / 1000 / NULLIF(CAST(total_duration_ms AS FLOAT) / 1000, 0), 0)) OVER () AS WarehouseAvgProportionTimeQueueing,
+    
+    -- Can use this to chargeback
+    CASE 
+        WHEN read_bytes > 0 
+        THEN CAST(read_bytes AS FLOAT) / (1024 * 1024 * 1024) 
+        ELSE 0 
+    END AS ReadDataAmountInGB,
+
+    CASE 
+        WHEN read_io_cache_percent > 0 THEN 'Used Cache' 
+        ELSE 'No Cache' 
+    END AS UsedCacheFlag,
+
+    CASE 
+        WHEN spilled_local_bytes > 0 THEN 'Spilled Data To Disk' 
+        ELSE 'No Spill' 
+    END AS HasSpillFlag,
+
+    CASE 
+        WHEN read_bytes > 0 THEN 'Did Read Data' 
+        ELSE 'No Data Read' 
+    END AS ReadDataFlag,
+
+    CASE 
+        WHEN COALESCE(CAST(total_task_duration_ms AS FLOAT) / 1000, 0) > 0 THEN 'UsedWorkerTasks' 
+        ELSE 'NoWorkers' 
+    END AS UsedWorkerTasksFlag,
+
+    -- Calculation Flags
+    CASE 
+        WHEN spilled_local_bytes > 0 THEN 1 
+        ELSE 0 
+    END AS Calc_HasSpillFlag,
+
+    CASE 
+        WHEN read_bytes > 0 THEN 0.25 
+        ELSE 0 
+    END AS Calc_ReadDataFlag,
+
+    CASE 
+        WHEN COALESCE(CAST(total_task_duration_ms AS FLOAT) / 1000, 0) > 0 THEN 0.25 
+        ELSE 0 
+    END AS Calc_UsedWorkerTasksFlag,
+
+    -- Query Tagging
+    REGEXP_REPLACE(
+        REGEXP_EXTRACT(statement_text, '/\\*.*?QUERY_TAG:(.*?)(?=\\*/)', 1), 
+        'QUERY_TAG:', ''
+    ) AS raw_tagged,
+
+    SUBSTR(statement_text, 
+           INSTR(statement_text, '/*') + 2, 
+           INSTR(statement_text, '*/') - INSTR(statement_text, '/*') - 2) AS dbt_metadata_json,
+
+    -- Optional DBT Metadata
+    COALESCE(dbt_metadata_json:app, 'None') AS dbt_app,
+    COALESCE(dbt_metadata_json:node_id, 'None') AS dbt_node_id,
+    COALESCE(dbt_metadata_json:profile_name, 'None') AS dbt_profile_name,
+    COALESCE(dbt_metadata_json:target_name, 'None') AS dbt_target_name,
+    COALESCE(dbt_metadata_json:dbt_version, 'None') AS dbt_version,
+    COALESCE(dbt_metadata_json:dbt_databricks_version, 'None') AS dbt_databricks_version,
+    FROM_JSON(dbt_metadata_json, 'map<string,string>') AS parsed_dbt_comment,
+
+    CASE 
+        WHEN (LOWER(dbt_metadata_json:app) = 'dbt' OR client_application LIKE '%dbt%') 
+        THEN 'DBT Query' 
+        ELSE 'Other Query Type' 
+    END AS IsDBTQuery
+
 FROM system.query.history
 WHERE compute.warehouse_id IS NOT NULL -- Only SQL Warehouse Compute
 AND statement_type IS NOT NULL
@@ -113,93 +154,93 @@ COMMENT 'SQL Warehouse SCD Change History'
 SCHEDULE CRON '0 0 0 * * ? *'
 TBLPROPERTIES ('pipelines.autoOptimize.zOrderCols'= 'event_time,warehouse_id')
 AS (
-
 WITH warehouse_raw_events AS (
-  
     SELECT 
-    event_time, 
-    workspace_id, 
-    account_id, 
-    action_name,
-    response,
-    request_params,
-    user_identity.email AS warehouse_editor_user
-    from system.access.audit
-    where service_name = 'databrickssql'
-    and action_name in ('createWarehouse', 'createEndpoint', 'editWarehouse', 'editEndpoint', 'deleteWarehouse', 'deleteEndpoint')
+        event_time, 
+        workspace_id, 
+        account_id, 
+        action_name,
+        response,
+        request_params,
+        user_identity.email AS warehouse_editor_user
+    FROM system.access.audit
+    WHERE service_name = 'databrickssql'
+    AND action_name IN ('createWarehouse', 'createEndpoint', 'editWarehouse', 'editEndpoint', 'deleteWarehouse', 'deleteEndpoint')
     AND response.status_code = '200'
 ),
 
 edit_history AS (
-    
     SELECT 
-    event_time, 
-    workspace_id, 
-    account_id, 
-    action_name,
-    from_json(response ['result'], 'Map<STRING, STRING>') ["id"] as warehouse_id, 
-    request_params.name as warehouse_name, 
-    request_params.warehouse_type AS warehouse_type,
-    request_params.auto_stop_mins AS auto_stop_mins,
-    request_params.cluster_size AS warehouse_size,
-    request_params.min_num_clusters::int AS min_cluster_scaling,
-    request_params.max_num_clusters::int AS max_cluster_scaing,
-    request_params.channel:name AS warehouse_channel,
-    warehouse_editor_user
-    from warehouse_raw_events
-    WHERE action_name in ('createWarehouse', 'createEndpoint')
-    QUALIFY
-    ROW_NUMBER() OVER (
+        event_time, 
+        workspace_id, 
+        account_id, 
+        action_name,
+        FROM_JSON(response['result'], 'Map<STRING, STRING>')['id'] AS warehouse_id, 
+        request_params.name AS warehouse_name, 
+        request_params.warehouse_type AS warehouse_type,
+        request_params.auto_stop_mins AS auto_stop_mins,
+        request_params.cluster_size AS warehouse_size,
+        CAST(request_params.min_num_clusters AS INTEGER) AS min_cluster_scaling,
+        CAST(request_params.max_num_clusters AS INTEGER) AS max_cluster_scaing,
+        CAST(request_params.channel:name AS STRING) AS warehouse_channel,
+        warehouse_editor_user
+    FROM warehouse_raw_events
+    WHERE action_name IN ('createWarehouse', 'createEndpoint')
+    QUALIFY ROW_NUMBER() OVER (
         PARTITION BY warehouse_id
-        ORDER BY
-          event_time DESC
-      ) = 1
+        ORDER BY event_time DESC
+    ) = 1
 
-      UNION ALL 
+    UNION ALL 
 
-      SELECT 
-      event_time, 
-      workspace_id, 
-      account_id, 
-      action_name,
-      request_params.id as warehouse_id,
-      request_params.name as warehouse_name,
-      request_params.warehouse_type AS warehouse_type,
-      request_params.auto_stop_mins AS auto_stop_mins,
-      request_params.cluster_size AS warehouse_size,
-      request_params.min_num_clusters::int AS min_cluster_scaling,
-      request_params.max_num_clusters::int AS max_cluster_scaing,
-      request_params.channel:name AS warehouse_channel,
-      warehouse_editor_user
-      from warehouse_raw_events
-      where
-      action_name in ('editWarehouse', 'editEndpoint')
+    SELECT 
+        event_time, 
+        workspace_id, 
+        account_id, 
+        action_name,
+        request_params.id AS warehouse_id,
+        request_params.name AS warehouse_name,
+        request_params.warehouse_type AS warehouse_type,
+        request_params.auto_stop_mins AS auto_stop_mins,
+        request_params.cluster_size AS warehouse_size,
+        CAST(request_params.min_num_clusters AS INTEGER) AS min_cluster_scaling,
+        CAST(request_params.max_num_clusters AS INTEGER) AS max_cluster_scaing,
+        CAST(request_params.channel:name AS STRING) AS warehouse_channel,
+        warehouse_editor_user
+    FROM warehouse_raw_events
+    WHERE action_name IN ('editWarehouse', 'editEndpoint')
 
     UNION ALL
+
     SELECT 
-    event_time, 
-    workspace_id, 
-    account_id, 
-    action_name,
-    request_params.id as warehouse_id,
-    NULL as warehouse_name, 
-    NULL AS warehouse_type,
-    NULL AS auto_stop_mins,
-    NULL AS warehouse_size,
-    NULL AS min_cluster_scaling,
-    NULL AS max_cluster_scaing,
-    NULL AS warehouse_channel,
-    warehouse_editor_user
-    from warehouse_raw_events
-    where
-    action_name in ('deleteWarehouse', 'deleteEndpoint')
+        event_time, 
+        workspace_id, 
+        account_id, 
+        action_name,
+        request_params.id AS warehouse_id,
+        NULL AS warehouse_name, 
+        NULL AS warehouse_type,
+        NULL AS auto_stop_mins,
+        NULL AS warehouse_size,
+        NULL AS min_cluster_scaling,
+        NULL AS max_cluster_scaing,
+        NULL AS warehouse_channel,
+        warehouse_editor_user
+    FROM warehouse_raw_events
+    WHERE action_name IN ('deleteWarehouse', 'deleteEndpoint')
 )
 
-SELECt *,
-ROW_NUMBER() OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time) AS ChangeRank,
-ROW_NUMBER() OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time DESC) AS RecencyRank,
-CASE WHEN RecencyRank = 1 AND action_name in ('deleteWarehouse', 'deleteEndpoint') THEN 'Deleted' ELSE 'Active' END AS IsDeleted
+SELECT 
+    *,
+    ROW_NUMBER() OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time) AS ChangeRank,
+    ROW_NUMBER() OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time DESC) AS RecencyRank,
+    CASE 
+        WHEN RecencyRank = 1 AND action_name IN ('deleteWarehouse', 'deleteEndpoint') 
+        THEN 'Deleted' 
+        ELSE 'Active' 
+    END AS IsDeleted
 FROM edit_history
+
 );
 
 
@@ -210,24 +251,27 @@ COMMENT 'SQL Warehouse Current Definition - including deleted flag'
 SCHEDULE CRON '0 0 0 * * ? *'
 TBLPROPERTIES ('pipelines.autoOptimize.zOrderCols'= 'event_time,warehouse_id')
 AS (
-
 WITH active_warehouses AS (
-select --warehouse_id, warehouse_name 
-*,
-MIN_BY(warehouse_editor_user, event_time) OVER (PARTITION BY account_id, workspace_id, warehouse_id) as warehouse_creator,
-MAX_BY(warehouse_editor_user, event_time) OVER (PARTITION BY account_id, workspace_id, warehouse_id) as warehouse_latest_editor
-from (SELECT * FROM main.dbsql_warehouse_advisor.warehouse_scd WHERE action_name in ('createWarehouse', 'createEndpoint', 'editWarehouse', 'editEndpoint')) -- Exclude delete events
-QUALIFY (ROW_NUMBER() OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time DESC) = 1)
+    SELECT 
+        *,
+        FIRST_VALUE(warehouse_editor_user) OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time ASC) AS warehouse_creator,
+        LAST_VALUE(warehouse_editor_user) OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS warehouse_latest_editor
+    FROM main.dbsql_warehouse_advisor.warehouse_scd 
+    WHERE action_name IN ('createWarehouse', 'createEndpoint', 'editWarehouse', 'editEndpoint') -- Exclude delete events
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY account_id, workspace_id, warehouse_id ORDER BY event_time DESC) = 1
 ),
 
 with_delete_flag AS (
-SELECT *,
-CASE WHEN (SELECT COUNT(0) 
-        from main.dbsql_warehouse_advisor.warehouse_scd AS innr
-        where
-         action_name in ('deleteWarehouse', 'deleteEndpoint')
-        AND innr.warehouse_id = aw.warehouse_id ) > 0 THEN 'Deleted' ELSE 'Active' END AS IsDeletedCurrently
-FROM active_warehouses AS aw
+    SELECT *,
+    CASE 
+        WHEN (SELECT COUNT(*) 
+              FROM main.dbsql_warehouse_advisor.warehouse_scd AS innr
+              WHERE action_name IN ('deleteWarehouse', 'deleteEndpoint')
+              AND innr.warehouse_id = aw.warehouse_id
+             ) > 0 THEN 'Deleted' 
+        ELSE 'Active' 
+    END AS IsDeletedCurrently
+    FROM active_warehouses AS aw
 )
 
 SELECT * FROM with_delete_flag
